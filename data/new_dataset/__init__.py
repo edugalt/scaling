358c43a049207bc95461121f824ca1367779cbb4
Eduardo G. Altmann <ega@math.usyd.edu.au>
2020-06-12 09:59:27 +1000

    New folder structure

---
+++ data/new_dataset/__init__.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+def index():
+    v = []
+    with open('../data/new_dataset/generic_dataset.txt', 'r') as f:
+        for line in f:
+            if line[0] == '#':
+                continue
+            line = line.strip("\n").split(',')  # split data
+            v.append(list(map(int, line[1:])))
+    v = np.array(v).transpose()
+    return v[0, :], v[1, :]
